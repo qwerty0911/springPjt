bf245963065260b1cf7b41776f6a511e2f8f7c37
BK <qwerty0911@gachon.ac.kr>
2023-01-04 16:25:59 +0900

    회원 서비스

---
+++ src/test/java/com/example/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,78 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.AbstractStringAssert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeAll
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member2 = new Member();
+        member2.setName("hello");
+
+        Member member1 = new Member();
+        member1.setName("hello");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+
+//        try {
+//
+//        }catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+//        memberService.join(member2);
+
+        //then
+//        Member findMember = memberService.findOne(saveId).get();
+//        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}

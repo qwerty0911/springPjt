ce2d8b968c79d93cd0d31d4ff4504b829b3eae85
BK <qwerty0911@gachon.ac.kr>
2023-01-05 15:05:39 +0900

    회원 페이지 구현

---
+++ src/main/java/com/example/hellospring/service/SpringConfig.java
@@ -0,0 +1,19 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}

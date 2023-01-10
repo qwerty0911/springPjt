812eb954d1344c1c74ebc6da0f0c4dbb5abead2e
BK <qwerty0911@gachon.ac.kr>
2023-01-10 16:09:39 +0900

    JPA 구현채 추가

--- src/main/java/com/example/hellospring/service/SpringConfig.java
+++ src/main/java/com/example/hellospring/service/SpringConfig.java
@@ -1,19 +1,25 @@
 package com.example.hellospring.service;
 
-import com.example.hellospring.repository.JdbcMemberRepository;
-import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
+import com.example.hellospring.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+//    private DataSource dataSource;
+    private EntityManager em;
+
+//        public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    @Autowired
+    public SpringConfig(EntityManager em){
+        this.em = em;
     }
 
     @Bean
@@ -24,7 +30,9 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
 
